e171746096df5add26afd6a47e437af66a3c79ab
Gayatri Naphade <gaytrinaphade@gmail.com>
2026-07-07 11:59:52 +0530

    Add files via upload
    Variables and operators are used to complete program

---
+++ variable-operators.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int marks = 44;
+
+        if (marks >= 90 && marks <= 100)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (marks >= 80)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (marks >= 70)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (marks >= 60)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else if (marks >= 40)
+        {
+            Console.WriteLine("Grade: E");
+        }
+        else if (marks >= 0)
+        {
+            Console.WriteLine("Grade: F (Fail)");
+        }
+        else
+        {
+            Console.WriteLine("Invalid marks!");
+        }
+    }
+}
+
+}
