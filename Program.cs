4a2a12cb0dd58c792423d2ee480ba2881b185c14
Gayatri Naphade <gaytrinaphade@gmail.com>
2026-07-08 19:42:20 +0530

    Add files via upload
    Added C# List Program.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+
+
+        List<int> num = new List<int>();
+        num.Add(12);
+        num.Add(14);
+        num.Add(662);
+        num.Add(92);
+        num.Add(55);
+        num.Add(67);
+
+        foreach(int n in num)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
